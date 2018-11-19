d215c336e3d247c56e3b600b66bbf073c5fb0deb
Paulo Vieira <pvieira@ID17038.example.com>
2018-11-19 04:14:23 +0000

    revert fromdate todate

--- plugins/readings/readings.js
+++ plugins/readings/readings.js
@@ -190,17 +190,17 @@ exports.register = function (server, options, next){
             let dbOptions = request.query;
             console.log(dbOptions);
 
-            let YYYY = dbOptions.fromDate.getFullYear();
-            let MM = dbOptions.fromDate.getMonth();
-            let DD = dbOptions.fromDate.getDate();
-            let dateTemp = new Date(YYYY, MM, DD);
-            dbOptions.fromDate = dateTemp.toISOString();
+//            let YYYY = dbOptions.fromDate.getFullYear();
+//            let MM = dbOptions.fromDate.getMonth();
+//            let DD = dbOptions.fromDate.getDate();
+//            let dateTemp = new Date(YYYY, MM, DD);
+//            dbOptions.fromDate = dateTemp.toISOString();
 
-            YYYY = dbOptions.toDate.getFullYear();
-            MM = dbOptions.toDate.getMonth();
-            DD = dbOptions.toDate.getDate();
-            dateTemp = new Date(YYYY, MM, DD);
-            dbOptions.toDate = dateTemp.toISOString();
+//            YYYY = dbOptions.toDate.getFullYear();
+//            MM = dbOptions.toDate.getMonth();
+//            DD = dbOptions.toDate.getDate();
+//            dateTemp = new Date(YYYY, MM, DD);
+//            dbOptions.toDate = dateTemp.toISOString();
 
 
             console.log('stringify:\n: ', JSON.stringify(dbOptions))
